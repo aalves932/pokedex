096a1b8052be822458b5b98f30d3ced987e9b91b
Andre Alves <andre.alves@sixsigma.com.br>
2023-01-09 02:11:46 -0300

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,18 @@
 <div class="p-8 max-w-6xl mx-auto">
     <Nav/>
 
+	<div class="leading-normal text-center text-sm text-slate-500 mt-3">
+		© made with ♥️<i class="fa fa-heart" /> by
+		<a
+			href="https://www.sixsigma.com.br"
+			class="font-semibold text-red-900"
+			target="_blank"
+			rel="noreferrer">Sixsigma Technologies</a
+		>
+	</div>
+
+	
+
     <div class="leading-normal text-center text-sm text-slate-500 lg:text-center m-3">
         © made with <i class="ni leading-none ni-favourite-28 text-lg relative top-0.3 text-red-700" /> by
         <a
