912aae9fdac818ee0f28bdb47c1040d63bf7c0f8
Andre Alves <andre.alves@sixsigma.com.br>
2023-01-09 02:35:27 -0300

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,12 @@
 <script>
     import "../app.css"
     import Nav from "../components/nav.svelte"
-    import '../lib/assets/css/nucleo-icons.css';
+    import '../lib/assets/css/nucleo-icons.css'
 
 </script>
 
 <div class="p-8 max-w-6xl mx-auto">
     <Nav/>
-
-	<div class="leading-normal text-center text-sm text-slate-500 mt-3">
-		© made with ♥️<i class="fa fa-heart" /> by
-		<a
-			href="https://www.sixsigma.com.br"
-			class="font-semibold text-red-900"
-			target="_blank"
-			rel="noreferrer">Sixsigma Technologies</a
-		>
-	</div>
-
-	
 
     <div class="leading-normal text-center text-sm text-slate-500 lg:text-center m-3">
         © made with <i class="ni leading-none ni-favourite-28 text-lg relative top-0.3 text-red-700" /> by
